771e4a4b6f45139d156955a6b1136bcc798dc8d3
Aron Adams <aronrapetto@hotmail.com>
2022-10-23 17:41:10 -0300

    feat: loginController

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import CustomizedError from '../utils/customizedError';
+import ILoginService from '../interfaces/service/login';
+
+export default class LoginController {
+  private loginService: ILoginService;
+
+  constructor(loginService: ILoginService) {
+    this.loginService = loginService;
+  }
+
+  public async login(req: Request, res: Response) {
+    const { body } = req;
+    if (!body.email || !body.password) throw new CustomizedError(400, 'All fields must be filled');
+
+    const token = await this.loginService.login(body);
+    res.status(200).json({ token });
+  }
+}
