ecee2fa554e547bfcdc9e64b16860d6274ca06ba
Aron Adams <aronrapetto@hotmail.com>
2022-10-23 19:31:38 -0300

    feat: LoginController.validate

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import IPayloadToken from '../interfaces/payload/payloadToken';
 import CustomizedError from '../utils/customizedError';
 import ILoginService from '../interfaces/service/login';
 
@@ -15,5 +16,12 @@ export default class LoginController {
 
     const token = await this.loginService.login(body);
     res.status(200).json({ token });
+  }
+
+  public validate(req: Request, res: Response) {
+    const { user } = req.body;
+
+    const role = this.loginService.validate(user as IPayloadToken);
+    res.status(200).json({ role });
   }
 }
