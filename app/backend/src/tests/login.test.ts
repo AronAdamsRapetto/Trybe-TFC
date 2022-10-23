071cb8408b85d380586753d1eebf6d9c26c66ea0
Aron Adams <aronrapetto@hotmail.com>
2022-10-23 20:47:46 -0300

    tests: description of tests refactored

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -28,7 +28,7 @@ describe('Testes de integração do login', () => {
   
     afterEach(() => sinon.restore());
   
-    it('Testa se ao mandar uma requisição sem email é retornado um erro 400', async () => {
+    it('Testa se o endpoint POST /login retorna um status 400 ao mandar uma request sem email', async () => {
       response = await chai.request(app).post('/login').send({
           password: 'secret_admin'
         });
@@ -37,7 +37,7 @@ describe('Testes de integração do login', () => {
       expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
     });
   
-    it('Testa se ao mandar uma requisição sem password é retornado um erro 400', async () => {
+    it('Testa se o endpoint POST /login retorna um status 400 ao mandar uma request sem password', async () => {
       response = await chai.request(app).post('/login').send({
           email: 'email@email.com',
         });
@@ -46,7 +46,7 @@ describe('Testes de integração do login', () => {
       expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
     });
   
-    it('Testa se ao mandar uma requisição com password inválido retorna status 401', async () => {
+    it('Testa se o endpoint POST /login retorna status 401 ao mandar uma request com password inválido', async () => {
       response = await chai.request(app).post('/login').send({
           email: 'admin@admin.com',
           password: '123456',
@@ -56,7 +56,7 @@ describe('Testes de integração do login', () => {
       expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
     });
   
-    it('Testa se ao mandar uma requisição com email inválido retorna status 401', async () => {
+    it('Testa se o endpoint POST /login retorna status 401 ao mandar uma request com email inválido', async () => {
       sinon.restore();
       sinon.stub(User, "findOne").resolves(null);
 
@@ -69,7 +69,7 @@ describe('Testes de integração do login', () => {
       expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
     });
   
-    it('Testa se ao mandar uma requisição correta retorna status 200', async () => {
+    it('Testa se o endpoint POST /login retorna status 200 ao mandar uma request correta', async () => {
       response = await chai.request(app).post('/login').send({
           email: 'admin@admin.com',
           password: 'secret_admin',
@@ -81,7 +81,7 @@ describe('Testes de integração do login', () => {
   });
 
   describe('Testes da rota "/login/validate"', () => {
-    it('Testa se ao validar login com token inválido retorna o erro 401', async () => {
+    it('Testa se o endpoint /login/validate retorna o status 401 ao mandar uma request com token inválido', async () => {
       response = await chai.request(app).get('/login/validate')
         .set('authorization', 'token inválido');
   
@@ -89,14 +89,14 @@ describe('Testes de integração do login', () => {
       expect(response.body).to.have.property('message');
     });
   
-    it('Testa se ao validar login sem o token retorna o erro 401', async () => {
+    it('Testa se o endpoint /login/validate retorna o status 401 ao mandar uma request sem o token', async () => {
       response = await chai.request(app).get('/login/validate');
   
       expect(response.status).to.be.equal(401);
       expect(response.body).to.be.deep.equal({ message: 'Invalid Token!' });
     });
   
-    it('Testa se é pissível validar login com o token correto', async () => {
+    it('Testa se o endpoint /login/validate retorna o status 200 ao mandar uma request com o token correto', async () => {
       sinon.stub(User, "findOne").resolves({
         id: 1,
         username: 'Admin',
